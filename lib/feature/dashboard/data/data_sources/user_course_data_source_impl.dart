22408f514f2fa092a6dc0fcefc2c378e31c8d034
Mozammal Hossain <144788896+Mozammal-hossain@users.noreply.github.com>
2023-12-06 18:43:45 +0600

    hotfix: Deleted necessary changes for asset remove

--- lib/feature/dashboard/data/data_sources/user_course_data_source_impl.dart
+++ lib/feature/dashboard/data/data_sources/user_course_data_source_impl.dart
@@ -10,7 +10,7 @@ class UserCourseDataSourceImp extends UserCourseDataSource {
         imageUrl: 'assets/images/react_js_course_cover.png',
         chapter: 13,
         finished: 0.0,
-        item: 4,
+        item: 2,
         playlist: [
           VideoEntity(
             'assets/videos/ReactJSModule_0.mp4',
@@ -24,18 +24,6 @@ class UserCourseDataSourceImp extends UserCourseDataSource {
             2,
             'Introduction',
           ),
-          VideoEntity(
-            'assets/videos/ReactJSModule_2.mp4',
-            'ESLint, Formatting with Prettier & Project Setup - React Tutorial Bangla Series',
-            3,
-            'Introduction',
-          ),
-          VideoEntity(
-            'assets/videos/ReactJSModule_3.mp4',
-            'How to install react in 2023 - - React Tutorial Bangla Series',
-            4,
-            'Introduction',
-          ),
         ],
       ),
       CourseModel(
@@ -43,8 +31,8 @@ class UserCourseDataSourceImp extends UserCourseDataSource {
         category: 'Programming',
         imageUrl: 'assets/images/javascript_course_cover.png',
         chapter: 12,
-        finished: 0,
-        item: 3,
+        finished: 0.0,
+        item: 2,
         playlist: [
           VideoEntity(
             'assets/videos/ReactJSModule_0.mp4',
@@ -55,19 +43,7 @@ class UserCourseDataSourceImp extends UserCourseDataSource {
           VideoEntity(
             'assets/videos/ReactJSModule_1.mp4',
             'React Virtual DOM Explained - Easy Explanation - React Tutorial Bangla Series',
-            1,
-            'Introduction',
-          ),
-          VideoEntity(
-            'assets/videos/ReactJSModule_2.mp4',
-            'ESLint, Formatting with Prettier & Project Setup - React Tutorial Bangla Series',
-            1,
-            'Introduction',
-          ),
-          VideoEntity(
-            'assets/videos/ReactJSModule_3.mp4',
-            'How to install react in 2023 - - React Tutorial Bangla Series',
-            1,
+            2,
             'Introduction',
           ),
         ],
